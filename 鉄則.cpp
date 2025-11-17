5f16d5d5bcbd188fa596d3626fbca86e05c0d4aa
chani0422 <117498306+chani0422@users.noreply.github.com>
2025-11-17 19:55:51 +0900

    Add files via upload

---
+++ 鉄則.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define pb(x) push_back(x)
+#define popb pop_back()
+#define fi first
+#define se second
+#define yn(x) do { if(x) cout << "Yes\n"; else cout << "No\n"; } while(0)
+#define nall(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define For(x) for(auto a : x)cout << a << " ";
+using ll = long long; using ld = long double;
+using Graph = vector<vector<char>>;
+
+//鉄則問題
+int main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    
+    return 0;
+}
